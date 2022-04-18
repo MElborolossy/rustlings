2e8f31555e97c4b5e1519738941e1878df2fd87d
melborolossy <mohammedelborolossy@gmail.com>
2022-04-18 02:55:50 +0200

    Day 1

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,16 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
-    println!("Number plus two is : {}", number + 2);
+    // Shadowing
+    {
+        let number = 3;
+        println!("Number plus two is : {}", number + 2);
+
+    }
+    // Assert to check that the shadowing is working
+    assert!(number == "T-H-R-E-E");
 }
